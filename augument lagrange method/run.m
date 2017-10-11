ccff2ea98da8d9d0cffc9f18ef9a2145bb292f09
jiawenhao2015 <1490776271@qq.com>
2017-10-11 14:42:54 +0800

    little change

--- augument lagrange method/run.m
+++ augument lagrange method/run.m
@@ -1,9 +1,10 @@
-
-
+close all;
+clear;
+clc;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for lambda1 = 1 : 1 : 1000
-    for lambda2 = 1 : 1: 1000
-        for beta = [0.00001,0.01]
+for lambda1 = 1 : 50 : 1000
+    for lambda2 = 1 : 50: 1000
+        for beta = [0.0001,0.01]
             solveEquation(lambda1,lambda2,beta);
         end        
     end
