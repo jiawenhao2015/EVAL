99da4c90c5fbe0fd8691d7097b3463d25311f19d
jiawenhao2015 <1490776271@qq.com>
2017-10-23 11:59:39 +0800

    求解误差值

---
+++ matlab/mainFunc.m
@@ -0,0 +1,8 @@
+function ret = mainFunc(X,Y,B,C,lambda1,lambda2)
+
+item1 = norm(Y-X*B,'fro');
+item2 = norm(B,1);
+item3 = norm( X*B(:,1:2) - X*B(:,3:4) - C(:,1:2),'fro');
+item4 = norm( X*B(:,5:6) - X*B(:,7:8) - C(:,3:4),'fro');
+
+ret = item1*item1 + lambda1*item2 + lambda2*(item3*item3 +item4*item4);
